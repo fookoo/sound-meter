67dfa260d7fab314d7525b6543bcb81a85dca708
Piotr Pliszczynski <ppliszczynski@artegence.com>
2015-01-07 11:49:12 +0100

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # sound-meter
-sound level meter js libray
+sound level meter js library
+
+it was designed for detecting "blow" or just high level on mic.
+
+## usage
+create instance of soundMeterCtrl
+
+> var instance = new soundMeterCtrl ();
+
+and then listen to events on document element.
+
+## events
+
+### sound.init.ok
+
+this event means that sound initialization goes ok, and user allow us to usemicrophone
+
+### sound.init.fail
+
+this event means that sound initialization goes wrong, or user declaim access to microphone
+
+### sound.blow.start
+
+this event means that sound level is higher than minimal threshold, this threshold could be set by minSoundLevel param
+
+### sound.blow.during
+
+this event means that sound level is higher than minimal threshold and sound.blow.start was already triggered
+
+### sound.blow.stop
+
+this event means that sound level dropped below minimal threshold
---
+++ sound-meter.js
@@ -0,0 +1,175 @@
+/* helpers */
+Array.prototype.average = function () {
+    var i = 0,
+        l = this.length,
+        s = 0;
+
+    for (i = 0; i < l; i++) {
+        s += this[i];
+    }
+
+    return s / l;
+};
+
+function soundMeterCtrl () {
+
+    var self = this;
+
+    //cross browser audio support
+    var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || false;
+    var audioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext || false;
+
+    var sound = {
+        on:                         false,  //if sound level is higher then min sound
+        enable:                     true,   //if sound events is triggered
+        level:                      0,      //raw level
+        normalized:                 0,      //normalized level (with dynamic noise level)
+        noise:                      0,      //raw noise level
+        minSoundLevel:              30,     //minmum sound level above noise level to trigger start event
+        minSoundLevel_normalized:   30,     //normalized minimum sound level above noise level to trigger start event [calculated, only initial]
+        max:                        60,
+        max_normalized:             60      //[calculated, only initial]
+    };
+
+
+    this.init = function () {
+        //soundMeter initialization
+        var initSuccessful  = new Event ('sound.init.ok'),
+            initFail        = new Event ('sound.init.fail');
+
+
+        var buffer_size     = 125,                               //how many samples take place in normalize process
+            buffer          = new Array (buffer_size),          //buffer for normalize level
+            sample          = 0,                                //sample count (iterator var)
+            inited          = false;                            //initial fill of buffer
+
+
+        if (typeof getUserMedia === "function") {
+            getUserMedia.call  (navigator,
+                {audio:true},
+                function(stream) {
+                    //Audio context initalization
+                    audioCtx        = new audioContext();
+                    analyser        = audioCtx.createAnalyser();
+                    microphone      = audioCtx.createMediaStreamSource(stream);
+                    javascriptNode  = audioCtx.createScriptProcessor(2048, 1, 1);
+
+                    analyser.smoothingTimeConstant = 0.8;
+                    analyser.fftSize = 1024;
+
+                    microphone.connect(analyser);
+                    analyser.connect(javascriptNode);
+                    javascriptNode.connect(audioCtx.destination);
+
+                    //reinit views for blowing version
+                    document.dispatchEvent (initSuccessful);
+
+                    //handle volume meter
+                    javascriptNode.onaudioprocess = function () {
+                        var array = new Uint8Array(analyser.frequencyBinCount);
+                        analyser.getByteFrequencyData(array);
+                        var values = 0;
+
+                        var length = array.length;
+                        for (var i = 0; i < length; i++) {
+                            values += (array[i]);
+                        }
+
+                        var average = values / length;
+
+                        sound.level = average;
+
+                        if (inited) {
+                            buffer = buffer.slice(1);
+                            buffer.push (average);
+
+                            sound.normalized = average - buffer.average() < 0 ? 0 : average - buffer.average();
+                            sound.noise = average - sound.normalized;
+
+                            sound.minSoundLevel_normalized = sound.minSoundLevel - ((sound.noise / 180) * sound.minSoundLevel);
+
+                            if (!sound.on) {
+                                sound.max_normalized = sound.max - sound.noise;
+                            }
+
+                        } else {
+                            buffer[sample++] = average;
+                        }
+
+                        if (!inited && sample > buffer_size) {
+                            sample = 0;
+                            inited = true;
+                        }
+
+                        self.checkLevels ();
+                    };
+                },
+                function () {
+                    document.dispatchEvent (initFail);
+                }
+            );
+        } else {
+            document.dispatchEvent (initFail);
+        }
+    };
+
+    /**
+     * check sound levels
+     *
+     * dispatch events
+     */
+    this.checkLevels = function () {
+        if (!sound.enable) {
+            return false;
+        }
+
+        //start
+        if (!sound.on && sound.normalized > sound.minSoundLevel_normalized ) {
+            sound.on = true;
+            sound.start = Date.now ();
+
+            var blowStart = new CustomEvent ('sound.blow.start', { detail: {
+                level: sound.level,
+                normalized: sound.normalized,
+                noise: sound.noise,
+                max: sound.max_normalized,
+                duration: 0
+            }});
+
+            document.dispatchEvent (blowStart);
+        }
+
+        //during
+        if (sound.on && sound.normalized > sound.minSoundLevel_normalized) {
+            sound.duration = Date.now () - sound.start;
+            var blowDuring = new CustomEvent ('sound.blow.during', { detail: {
+                level: sound.level,
+                normalized: sound.normalized,
+                noise: sound.noise,
+                max: sound.max_normalized,
+                duration: sound.duration
+            }});
+
+            document.dispatchEvent (blowDuring);
+        }
+
+        //stop
+        if (sound.on && sound.normalized < sound.minSoundLevel_normalized) {
+            sound.on = false;
+            sound.stop = Date.now ();
+            sound.duration = sound.stop - sound.start;
+
+            var blowStop = new CustomEvent ('sound.blow.stop', { detail: {
+                level: sound.level,
+                normalized: sound.normalized,
+                noise: sound.noise,
+                max: sound.max_normalized,
+                duration: sound.duration
+            }});
+
+            document.dispatchEvent (blowStop);
+        }
+    };
+
+    self.init ();
+};
